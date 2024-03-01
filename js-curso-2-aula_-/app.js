4163f3707720d09b7e074ad63959cac035a554d4
Felipe Ferraz <felipeferrazs@outlook.com>
2024-03-01 12:30:49 -0300

    Alterando o jogo de 10 números aleatórios, para 100 números

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumero();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirTexto(tag, texto){
 
 function mensagemInicial(){
     exibirTexto('h1', 'Jogo do Número Secreto');
-    exibirTexto('p', 'Escolha um número entre 1 a 10');
+    exibirTexto('p', 'Escolha um número entre 1 a 100');
 }
 
 
